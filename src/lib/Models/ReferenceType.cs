e65cc320958f855dedfd9e1d80c8ffbcaf0e35ea
Vincent Biret <vincentbiret@hotmail.com>
2026-01-24 20:29:23 -0500

    docs: adds missing doc comments
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- src/lib/Models/ReferenceType.cs
+++ src/lib/Models/ReferenceType.cs
@@ -2,14 +2,29 @@ using Microsoft.OpenApi;
 
 namespace BinkyLabs.OpenApi.Arazzo;
 
+/// <summary>
+/// The type of reference
+/// </summary>
 public enum ReferenceType
 {
+    /// <summary>
+    /// Success Action component type
+    /// </summary>
     [Display("successActions")]
     SuccessAction,
+    /// <summary>
+    /// Failure Action component type
+    /// </summary>
     [Display("failureActions")]
     FailureAction,
+    /// <summary>
+    /// Parameter component type
+    /// </summary>
     [Display("parameters")]
     Parameter,
+    /// <summary>
+    /// Input component type
+    /// </summary>
     [Display("inputs")]
     Input,
 
